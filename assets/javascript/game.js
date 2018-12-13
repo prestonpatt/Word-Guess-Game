35fa6dc37d6ca86c79cc2517837d590f2da2dd9e
Antifreeze <preston.patterson1@gmail.com>
2018-12-13 05:56:51 -0600

    more CSS, initial arrays setup, game start alert message

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,17 @@
+alert('\n\t\t\t\tWave your wand to start game!\n\n\t\t\t(Really just move your cursor and click OK)\n\n\tRead the instructions on the left before typing your next key.'
+
+)
+
 var alphabet = [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z]
+var answers = [bellatrix, dumbledore, gryffindor, harry, hermione, hippogriff, hogwarts, hufflepuff, mcgonagall, patronus, phoenix, quidditch, ravenclaw, ron, slytherin, snape, sortinghat, umbridge, voldemort, wand]
+var correctGuess = []
+var incorrectGuess = []
+var underscores = []
+var guesses = []
+var remainingGuesses = []
+var alreadyGuessed = []
+var wins = []
+var losses = []
+var mainWord = []
+
+
